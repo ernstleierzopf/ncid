8f028573f0621a9ea1068e2a5c6eea8e257bc511
eleierzopf <e.leierzopf@gmail.com>
2020-02-19 18:45:05 +0100

    added more arguments for generatePlainTextFiles. added fileUtils.

--- data/scripts/generate/generatePlainTextFiles.py
+++ data/scripts/generate/generatePlainTextFiles.py
@@ -1,12 +1,26 @@
 import argparse
+import os
+import sys
+
+sys.path.append("../../../")
+from utilities import fileUtils
 
 def str2bool(v):
     return v.lower() in ("yes", "true", "t", "1")
 
 parser = argparse.ArgumentParser(
     description='CANN Plaintext Generator Script')
-parser.add_argument('--input_folder', default='../../gutenberg_en', type=str,
+parser.add_argument('--input_folder', default='../../gutenberg_test', type=str,
                     help='Input folder of the plaintexts to be extracted.')
+parser.add_argument('--restructure_input_folder', default=False, type=str2bool,
+                    help='Deletes all unneeded files from the input_folder recursively '\
+                         'and moves the files into the first subdirectory.'\
+                         'This option can only be used with input_folder_structure=\'gutenberg\'.')
+parser.add_argument('--input_folder_structure', default=fileUtils.FOLDER_STRUCTURE[0],
+                    choices=fileUtils.FOLDER_STRUCTURE, help='Sets the structure of the input folder.'\
+                    'This is needed to be able to handle different directory structures and not to'\
+                    'reuse the same file in another format. Possible values are \'gutenberg\' for the'\
+                    'gutenberg-library or common, if you want to use all files in the subdirectories.')
 parser.add_argument('--save_folder', default='../../plaintexts',
                     help='Directory for saving extracted plaintexts.')
 parser.add_argument('--save_file_size', default=10, type=int,
@@ -23,11 +37,28 @@ parser.add_argument('--min_sentence_length', default=50, type=int,
                     help='Defines the minimal number of characters in a sentence to be chosen.')
 parser.add_argument('--max_sentence_length', default=150, type=int,
                     help='Defines the maximal number of characters in a sentence to be chosen.')
+parser.add_argument('--caches_file_name', default='../../caches.txt', type=str,
+                    help='Sets the filename of the caches file.')
+parser.add_argument('--remove_caches', default=False, type=str2bool,
+                    help='Removes all caches for the given input_folder and restarts the caching process.')
 
-
+#parser.set_defaults(keep_spaces=False, remove_caches=False)
 args = parser.parse_args()
-parser.set_defaults(keep_spaces=False)
+
+args.input_folder = os.path.abspath(args.input_folder)
+args.save_folder = os.path.abspath(args.save_folder)
+args.caches_file_name = os.path.abspath(args.caches_file_name)
+
+if args.restructure_input_folder and args.input_folder_structure != fileUtils.FOLDER_STRUCTURE[0]:
+    print("The parameter --restructure_input_folder can only be used with --input_folder_structure=%s"%fileUtils.FOLDER_STRUCTURE[0], file=sys.stderr)
 
 #print all arguments for debugging..
 for arg in vars(args):
     print("%s = %s"%(arg, vars(args)[arg]))
+
+if os.path.exists(args.caches_file_name) and not args.remove_caches:
+    print("An caches file already exists under '%s'. Please use the --remove_caches option to recreate the caches."%args.caches_file_name)
+else:
+    txt_files = fileUtils.find_textfiles(args.input_folder, args.input_folder_structure, args.restructure_input_folder)
+    txt_files = fileUtils.restructure_folder_and_write_caches(args.restructure_input_folder, txt_files, args.input_folder, args.caches_file_name)
+
---
+++ utilities/fileUtils.py
@@ -0,0 +1,102 @@
+import os
+
+FOLDER_STRUCTURE = ('gutenberg', 'common')
+
+def write_ciphertexts_with_keys_to_file(filename, ciphertexts, keys, cipherImplementation) :
+    with open(filename,'w') as file :
+        for i in range(0,len(ciphertexts)) :
+            file.write(ciphertexts[i] + ',' + cipherImplementation.mapNumbersIntoTextspace(keys[i]))
+            file.write('\n')
+
+def write_txt_list_to_file(filename, texts) :
+    with open(filename,'w') as file :
+        for line in texts :
+            file.write(line)
+            file.write('\n')
+
+def read_txt_list_from_file(filename):
+    with open(filename,'r') as file:
+        return file.readlines()
+
+'''
+This function searches recursively for all textfiles stored in the path and its sub-folders
+It stores the found filenames (full paths) in cache_file and returns the list of filenames.
+'''
+def find_textfiles(path, folder_structure, restructure_folder_flag) :
+    file_counter = 0
+    txt_files = []
+    if restructure_folder_flag:
+        unpack_zip_folders(path)
+    dir = os.listdir(path)
+    for name in dir:
+        if os.path.isdir(os.path.join(path, name)) :
+            txt_files += find_textfiles(os.path.join(path, name), folder_structure, restructure_folder_flag)
+        elif name.lower().endswith('.txt') and '-' not in name:
+            if os.path.join(path, name) not in txt_files:
+                txt_files.append(os.path.join(path, name))
+                remove_disclaimer_from_file(os.path.join(path, name))
+                file_counter = file_counter + 1
+            # console output for % of read files
+            if file_counter % 10 == 0 :
+                percentage = int( float(file_counter) / float(len(dir)) * 100)
+                output = 'Collecting files: ['
+                for i in range(0, int(percentage * 0.2)) :
+                    output += '#'
+                for i in range( int(percentage * 0.2), 20) :
+                    output += '.'
+                output = output + '] ' + str(file_counter) + ' from ' + str(path) + ' ('  + str(percentage) + "%)"
+                print(output)
+        elif restructure_folder_flag:
+            if os.path.exists(os.path.join(path, name)):
+                os.remove(os.path.join(path, name))
+    if os.path.exists(path) and len(os.listdir(path)) == 0:
+        os.removedirs(path)
+    return txt_files
+
+def restructure_folder_and_write_caches(restructure_folder_flag, txt_files, path, cache_file):
+    if restructure_folder_flag:
+        restructured_txt_files = []
+        for file in txt_files:
+            restructured_path = os.path.join(path, os.path.basename(file))
+            if restructured_path != file and os.path.exists(restructured_path):
+                print('Path \'%s\' already exists!'%restructured_path)
+            os.rename(file, restructured_path)
+            restructured_txt_files.append(restructured_path)
+            dir = os.path.dirname(file)
+            while len(os.listdir(dir)) == 0:
+                os.removedirs(os.path.dirname(file))
+                dir = os.path.dirname(dir)
+        txt_files = restructured_txt_files
+
+    if cache_file is not None:
+        write_txt_list_to_file(cache_file, txt_files)
+    return txt_files
+
+def unpack_zip_folders(path):
+    import zipfile
+    dir = os.listdir(path)
+    for name in dir:
+        if name.lower().endswith('.zip') and '-' not in name:
+            with zipfile.ZipFile(os.path.join(path, name), 'r') as zip_ref:
+                zip_ref.extractall(path)
+            if os.path.exists(os.path.join(path, name)):
+                os.remove(os.path.join(path, name))
+
+def remove_disclaimer_from_file(file):
+    with open(file, 'rb') as f:
+        txt = f.read()
+    txt.replace(b'\r\n', b'\n')
+    with open(file, 'wb') as f:
+        f.write(txt)
+
+    with open(file, 'r', encoding='ascii') as f:
+        txt = f.read()
+    pos = txt.find('*** START')
+    if pos < 0:
+        pos = txt.find('***START')
+        if pos < 0:
+            print('No disclaimer found in %s'%file)
+            return
+    txt = txt[pos:]
+    with open(file, 'w', encoding='ascii') as f:
+        f.write(txt)
