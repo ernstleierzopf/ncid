70960700a2fe478f4d8cb0e021feba13d10d35ac
eleierzopf <e.leierzopf@gmail.com>
2020-07-09 16:54:07 +0200

    removed plaintext cutting and added substitution of j and v in the baconian cipher.

--- cipherImplementations/baconian.py
+++ cipherImplementations/baconian.py
@@ -3,11 +3,10 @@ import random
 
 
 class Baconian(Cipher):
-    def __init__(self, alphabet, unknown_symbol, unknown_symbol_number, split_plaintext=True):
+    def __init__(self, alphabet, unknown_symbol, unknown_symbol_number):
         self.alphabet = alphabet
         self.unknown_symbol = unknown_symbol
         self.unknown_symbol_number = unknown_symbol_number
-        self.split_plaintext = split_plaintext
 
     def generate_random_key(self, length=None):
         return [[0,0,0,0,0], [0,0,0,0,1], [0,0,0,1,0], [0,0,0,1,1], [0,0,1,0,0], [0,0,1,0,1], [0,0,1,1,0], [0,0,1,1,1], [0,1,0,0,0],
@@ -15,15 +14,16 @@ class Baconian(Cipher):
                 [1,0,0,0,1], [1,0,0,1,0], [1,0,0,1,1], [1,0,0,1,1], [1,0,1,0,0], [1,0,1,0,1], [1,0,1,1,0], [1,0,1,1,1]]
 
     def encrypt(self, plaintext, key):
-        if self.split_plaintext:
-            plaintext = plaintext[:int(len(plaintext) / 5)]
         ciphertext = []
         for p in plaintext:
             for k in key[p]:
                 if k < 13:
-                    ciphertext.append(random.randint(0, 12))
+                    r = random.randint(0, 12)
                 else:
-                    ciphertext.append(random.randint(13, 25))
+                    r = random.randint(13, 25)
+                if r == 9 or 21:  # remove j and v
+                    r -= 1
+                ciphertext.append(r)
         return ciphertext
 
     def decrypt(self, ciphertext, key):
